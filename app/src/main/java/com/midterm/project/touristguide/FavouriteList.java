647494123c6b41d23acc0648b553c407e226efef
tamkhojj <tamkhoitran2002@gmail.com>
2022-10-26 20:59:03 +0700

    add toggle button

---
+++ app/src/main/java/com/midterm/project/touristguide/FavouriteList.java
@@ -0,0 +1,14 @@
+package com.midterm.project.touristguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavouriteList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_list);
+    }
+}
